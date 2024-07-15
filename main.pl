4a565a8739793e92030a3db75b6f7905a9bd5c27
catlenc <catlen.cleane@hotmail.com>
2024-07-15 14:49:09 -0300

    Comentario das possiveis funções

---
+++ main.pl
@@ -0,0 +1,13 @@
+# Base de dados dos filmes: Contém informações sobre os filmes, incluindo título, gênero, diretor, ano, ator principal, classificação, duração e país de produção.
+
+# Pontuação individual para cada critério: Calcula a pontuação de um filme para cada critério (gênero, diretor, ano, ator, classificação, duração, país).
+
+# Pontuação total: Calcula a pontuação total de um filme somando as pontuações individuais dos critérios.
+
+# Função para sugerir filmes: Gera uma lista de filmes ordenada pela pontuação total.
+
+# Interação com o usuário: Faz perguntas ao usuário para coletar suas preferências.
+
+# Função principal para executar o programa: Controla o fluxo do programa, desde a interação com o usuário até a exibição das sugestões de filmes.
+
+# Listar filmes sugeridos: Exibe a lista de filmes sugeridos ao usuário.
