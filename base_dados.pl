75b0efc04dca6be647a7a8bf8b3e9e104731ff53
catlenc <catlen.cleane@hotmail.com>
2024-07-16 10:57:44 -0300

    Função filtrar
    Co-authored-by: catlenc <catlen.cleane@hotmail.com>
    Co-authored-by: gabrielrosa09 <gabriel10919@outlook.com>

---
+++ base_dados.pl
@@ -0,0 +1,8 @@
+% Base de dados de filmes
+filme('Inception', 'christopher_nolan', 2010, 'leonardo_dicaprio', maiores_14, acao).
+filme('Titanic', 'james_cameron', 1997, 'leonardo_dicaprio', maiores_12, romance).
+filme('The Dark Knight', 'christopher_nolan', 2008, 'christian_bale', maiores_14, acao).
+filme('Pulp Fiction', 'quentin_tarantino', 1994, 'john_travolta', maiores_18, drama).
+filme('The Godfather', 'francis_ford_coppola', 1972, 'marlon_brando', maiores_18, drama).
+filme('Avatar', 'james_cameron', 2009, 'sam_worthington', maiores_12, ficcao_cientifica).
+
--- main.pl
+++ main.pl
@@ -1,32 +1,7 @@
-/*Base de dados dos filmes: Contém informações sobre os filmes, incluindo título, gênero, diretor, ano, ator principal, classificação, duração e país de produção.
+:- consult('base_dados.pl').
+:- consult('interacao.pl').
+:- consult('filtro.pl').
 
-
-Listar as opções de gênero, diretor, ano, ator principal, classificação.
-
-Interação com o usuário: Faz perguntas ao usuário para coletar suas preferências.*/
-
-%Pergunta ao usuário suas preferências de filmes
-perguntar_criterio(Genero, Diretor, Ano, Ator, Classificacao, Duracao, Pais) :-
-    write('Qual genero de filme voce prefere? (Digite "any" para qualquer genero): '), read(Genero),
-    write('Qual diretor voce prefere? (Digite "any" para qualquer diretor): '), read(Diretor),
-    write('Qual ano de lancamento voce prefere? (Digite "any" para qualquer ano): '), read(Ano),
-    write('Qual ator principal voce prefere? (Digite "any" para qualquer ator): '), read(Ator),
-    write('Qual classificacao voce prefere? (Digite "any" para qualquer classificacao): '), read(Classificacao),
- 
-
-%Predicado para iniciar o questionário
-iniciar_pergunta :-
-    perguntar_criterio(Genero, Diretor, Ano, Ator, Classificacao, Duracao, Pais),
-    format('Genero: ~w~n', [Genero]),
-    format('Diretor: ~w~n', [Diretor]),
-    format('Ano: ~w~n', [Ano]),
-    format('Ator: ~w~n', [Ator]),
-    format('Classificacao: ~w~n', [Classificacao]).
-
-
-
-
-
-/* Função principal para executar o programa: Controla o fluxo do programa, desde a interação com o usuário até a exibição das sugestões de filmes.*/
-
-/*Listar filmes sugeridos: Exibe a lista de filmes sugeridos ao usuário*/
+%Predicado para iniciar a aplicação
+iniciar :-
+    iniciar_pergunta.
