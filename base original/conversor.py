8625b480d987520d8af0d504f343e3da40a6b7b4
lorenzo7377 <lorenzodavinci@hotmail.com>
2024-07-16 12:40:40 -0300

    tratamento para listas

--- base original/conversor.py
+++ base original/conversor.py
@@ -1,8 +1,21 @@
 import csv
-# https://www.kaggle.com/datasets/stefanoleone992/filmtv-movies-dataset
+
 # Caminho para o arquivo CSV fornecido
 input_file_path = '../base original/filmtv_movies.csv'
-output_file_path = '../base original/filtered_movies.txt'
+output_file_path = '../base original/basededados.txt'
+
+# Função para adicionar aspas duplas em torno de cada termo em uma lista
+def add_quotes_and_format_list(list_string):
+    terms = [term.strip() for term in list_string.split(',')]
+    
+    if len(terms) > 0:
+        if len(terms) > 1:
+            quoted_terms = [f'"{term}"' for term in terms]
+            return f'[{", ".join(quoted_terms)}]'
+        else:
+            return f'"{terms[0]}"'
+    else:
+        return '"none"'
 
 # Ler o arquivo CSV original
 with open(input_file_path, 'r', newline='', encoding='utf-8') as csvfile:
@@ -16,10 +29,10 @@ with open(input_file_path, 'r', newline='', encoding='utf-8') as csvfile:
             year = row['year']
             duration = row['duration']
             country = row['country']
-            directors = row['directors']
-            actors = row['actors']
+            directors = add_quotes_and_format_list(row['directors'])
+            actors = add_quotes_and_format_list(row['actors'])
             
             # Escrever os valores em uma linha, separados por ";"
-            txtfile.write(f'filme("{title}","{genre}",{year},{duration},"{country}","{directors}","{actors}").\n')
+            txtfile.write(f'filme("{title}","{genre}",{year},{duration},"{country}",{directors},{actors}).\n')
 
 print("Arquivo criado com sucesso.")
