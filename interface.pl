42d297e9b0ae1435982395d435c456d6dcaf4686
Victor Cabral <87657291+victordscabral@users.noreply.github.com>
2024-07-17 22:55:55 -0300

    Versao mais recente da interface

--- interface.pl
+++ interface.pl
@@ -1,6 +1,7 @@
 :- use_module(library(pce)).
 :- [basededados].
 :- [listagem].
+:- [filtro].
 
 menu :-
     new(Dialog, dialog('Tela Ciano')),
@@ -8,16 +9,18 @@ menu :-
     send(Dialog, background, colour(cyan)),
     send(Dialog, append, new(T, text(''))),
     send(T, font, font(screen, bold, 26)),
-    mensagemInicial(Mensagem),
+    mensagem_inicial(Mensagem),
     send(T, string, Mensagem),
     
     new(Group, dialog_group(buttons, group)),
-    send(Group, append, button('Ir para Listagem', message(@prolog, listar, Dialog))),
+    send_list(Group, append,
+              [ button('Listagem', message(@prolog, listar, Dialog)),
+                button('Filtragem', message(@prolog, filtrar, Dialog))
+              ]),
     send(Dialog, append, Group),
-    
     send(Dialog, open_centered).
 
-mensagemInicial(Mensagem) :-
+mensagem_inicial(Mensagem) :-
     atomic_list_concat([
         'Olá, seja bem-vindo ao Movie Rescue, o programa que te ajudará a procurar seu novo filme favorito!\n\n',
         'Com esse programa, você verá dados de filmes buscando por atores, diretores, tempo de duração, gênero e nome.\n\n',
@@ -33,7 +36,7 @@ listar(Dialog) :-
     send(Dialog, append, new(Group, dialog_group(buttons, group))),
     send_list(Group, append,
               [ new(T, menu(topicos, cycle)),
-                button(voltar, message(@prolog, menu_inicial, Dialog)),
+                button(voltar, message(@prolog, retorna_menu, Dialog)),
                 button(pesquisar, and(message(@prolog, listar_recomendacoes, T?selection)))
               ]),
     opcoes_topicos(OP),
@@ -41,38 +44,51 @@ listar(Dialog) :-
     send(Dialog, layout_dialog),
     send(Dialog, layout).
 
-menu_inicial(Dialog) :-
+filtrar(Dialog) :-
+    send(Dialog, clear),
+    send(Dialog, append, new(Group, dialog_group(buttons, group))),
+    send_list(Group, append,
+              [ new(T, menu(filtros, cycle)),
+                new(Pesquisa, text_item(pesquisa, '')), % Novo campo de pesquisa
+                button(voltar, message(@prolog, retorna_menu, Dialog)),
+                button(aplicar, and(message(@prolog, aplicar_filtro, T?selection, Pesquisa?selection?get)))
+              ]),
+    opcoes_filtros(Filtros),
+    send_list(T, append, Filtros),
+    send(Dialog, layout_dialog),
+    send(Dialog, layout).
+
+retorna_menu(Dialog) :-
     send(Dialog, clear),
     send(Dialog, background, colour(cyan)),
     send(Dialog, append, new(T, text(''))),
     send(T, font, font(screen, bold, 26)),
-    mensagemInicial(Mensagem),
+    mensagem_inicial(Mensagem),
     send(T, string, Mensagem),
     
     new(Group, dialog_group(buttons, group)),
-    send(Group, append, button('Ir para Listagem', message(@prolog, listar, Dialog))),
+    send_list(Group, append,
+              [ button('Listagem', message(@prolog, listar, Dialog)),
+                button('Filtragem', message(@prolog, filtrar, Dialog))
+              ]),
     send(Dialog, append, Group),
-    
     send(Dialog, layout).
 
 opcoes_topicos(['Nome do Filme', 'Gênero', 'Diretor(a)', 'Ator/Atriz', 'Tudo']).
+opcoes_filtros(['Gênero', 'Ano do Filme', 'Duração', 'Diretor', 'Ator/Atriz']).
 
 listar_recomendacoes(Topico) :-
-    ( Topico == 'Nome do Filme' ->
-        listar_titulos
-    ; Topico == 'Gênero' ->
-        listar_generos
-    ; Topico == 'Diretor(a)' ->
-        listar_diretores
-    ; Topico == 'Ator/Atriz' ->
-        listar_elenco
-    ; Topico == 'Tudo' ->
-        todos_filmes(Todos),
-        mostrar_resultados(Todos)
-    ;   send(@display, inform, 'Selecione uma opção válida.')
+    ( Topico == 'Nome do Filme' -> listar_titulos
+    ; Topico == 'Gênero' -> listar_generos
+    ; Topico == 'Diretor(a)' -> listar_diretores
+    ; Topico == 'Ator/Atriz' -> listar_elenco
+    ; Topico == 'Tudo' -> todos_filmes(Todos), mostrar_resultados(Todos)
+    ; send(@display, inform, 'Selecione uma opção válida.')
     ).
 
-% Nessa parte em especifico utilizei como referencia o projeto YouTubeRecommendation: https://github.com/UnBParadigmas2023-2/2023.2_G2_Logico_YouTubeRecommendation/tree/main
+aplicar_filtro(Filtro, TermoPesquisa) :-
+    listar_filmes_por_criterio(Filtro, TermoPesquisa).
+
 mostrar_resultados(Resultados) :-
     new(Dialog, dialog('Resultados da Pesquisa')),
     new(Fonte, font(screen, bold, 12)),
--- main.pl
+++ main.pl
@@ -3,13 +3,6 @@
 
 main :-
     menu.
-    % Incluir os predicados de filtro e interação
-    :- consult('filtro.pl').
-    :- consult('interacao.pl').
-
 
 :- initialization(main).
-
-
-
 
